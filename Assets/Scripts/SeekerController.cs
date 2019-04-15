3af0a804d29dab865676bdcc44aa3ba67530d115
Nithya Natarajan <nithyanatarajn@gmail.com>
2019-04-15 23:01:20 +0530

    Move the seeker
    Signed-off-by: Nithya Natarajan <nithyanatarajn@gmail.com>

---
+++ Assets/Scripts/SeekerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SeekerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 1.0f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        var movement = new Vector3(moveHorizontal, 0.0f, moveVertical); // ball doesn't have to move up, so y=0
+        rb.AddForce(movement * speed);
+    }
+}
