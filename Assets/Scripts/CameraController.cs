6f406359aa9ff1ca6ebba93f2bafeb710ecf1d6a
Nithya Natarajan <nithyanatarajn@gmail.com>
2019-04-15 23:46:07 +0530

    Add camera position
    Signed-off-by: Nithya Natarajan <nithyanatarajn@gmail.com>

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject seeker;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - seeker.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = seeker.transform.position + offset;
+    }
+}
