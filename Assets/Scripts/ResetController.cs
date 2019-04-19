3caeded0aeccd942f03abd0445ac9d369fe46e2f
Nithya Natarajan <nithyanatarajn@gmail.com>
2019-04-19 11:42:09 +0530

    Add reset button
    Signed-off-by: Nithya Natarajan <nithyanatarajn@gmail.com>

---
+++ Assets/Scripts/ResetController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ResetController : MonoBehaviour
+{
+    public Button resetButton;
+
+    private void Start()
+    {
+        resetButton.onClick.AddListener(TaskOnClick);
+    }
+
+    private void TaskOnClick()
+    {
+        SceneManager.LoadScene("GameArea");
+    }
+}
